e5575457a185328b3ae4845bf0c63b8f35d76c65
eccentricnode <austin.b.johnson98@gmail.com>
2025-04-13 19:20:44 -0700

    should return book internals

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_book_text(file_path):
+    print(file_path)
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    get_book_text("./books/frankenstein.txt")
+
+main()
