9af99418952e6fb63db5a2f376fe20a4b66b15fb
eccentricnode <austin.b.johnson98@gmail.com>
2025-04-13 20:01:36 -0700

    refactor: moved count to stats file

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-def get_book_text(file_path):
-    print(file_path)
-    with open(file_path) as f:
-        file_contents = f.read()
-    return file_contents
+from stats import get_words_number
 
 def main():
-    get_book_text("./books/frankenstein.txt")
+    book_path = "./books/frankenstein.txt"
+    text = get_book_text(book_path)
+    number_words = get_words_number(text)
+    print(f"{number_words} words found in the document")
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+
 
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_words_number(text):
+    words_number = text.split()
+    return len(words_number)
+
